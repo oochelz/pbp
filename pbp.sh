90e9ba7ac7ac7e722c0d484c882b8b15e2347404
Chelsea Youmans <chelsea.youmans@careerbuilder.com>
2014-04-08 17:41:49 -0400

    Load the site once when done

--- pbp.sh
+++ pbp.sh
@@ -2,17 +2,24 @@ echo "$(tput setaf 5)Pulling and bundling js-global...$(tput sgr0)"
 cd js-global
 git pull
 bundle install
+
 echo "$(tput setaf 5)Pulling and bundling consumer-domain...$(tput sgr0)"
 cd ../consumer-domain
 git pull
 bundle install
+
 echo "$(tput setaf 5)Pulling and bundling CB-Mobile...$(tput sgr0)"
 cd ../CB-Mobile
 git pull
 bundle install
+
 echo "$(tput setaf 5)Powdering down and powdering up...$(tput sgr0)"
 powder down
 powder up
+
+echo "$(tput setaf 5)Hitting http://cb-mobile.dev so you don't have to wait for it to startup...$(tput sgr0)"
+wget --quiet http://cb-mobile.dev
+
 echo "$(tput setaf 5)We're done here. Checkout this sweet turtle."
 echo "$(tput setaf 2)"
 cat <<"EOT"
