ce482c1b1d5a0612fe0d60de2b52f4adf5d1b805
usbsnowcrash <jeffery.yeary@careerbuilder.com>
2014-11-20 16:40:07 -0500

    warm up apps

--- pbp.sh
+++ pbp.sh
@@ -37,6 +37,10 @@ echo "$(tput setaf 5)Powdering down and powdering up...$(tput sgr0)"
 powder down
 powder up
 
+echo "$(tput setaf 5)Warming up domain-values and consumer-domain...$(tput sgr0)"
+curl http://domain-values.dev?host=m.careerbuilder.dev >/dev/null 2>&1
+curl http://consumer-domain.dev/jobs/search?bid=hello >/dev/null 2>&1
+
 echo "$(tput setaf 5)We're done here. Checkout this sweet turtle and robot."
 echo "$(tput setaf 2)"
 cat <<"EOT"
